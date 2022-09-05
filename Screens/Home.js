a9febb5e325723c4e13447ea3926155cb9344f23
Mandeep <mandeep.windsor@gmail.com>
2022-09-04 21:46:12 -0700

    Set the routes for the app and initialized the expo env.

---
+++ Screens/Home.js
@@ -0,0 +1,20 @@
+import {
+  View,
+  Text,
+  FlatList,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  Keyboard,
+} from "react-native";
+import React, { useEffect, useState } from "react";
+
+const Home = () => {
+  return (
+    <View>
+      <Text>Home</Text>
+    </View>
+  );
+};
+
+export default Home;
